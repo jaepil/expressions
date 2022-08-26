1af4c18a792c1e8c620efb4d384c6b4fd05b4f87
Jaepil Jeong <jaepil@deepsearch.com>
2022-08-25 17:32:30 -0700

    Update main.cpp

--- src/expressions/main.cpp
+++ src/expressions/main.cpp
@@ -1,6 +1,30 @@
+//
+// Expressions
+//
+// Copyright (c) 2022 Jaepil Jeong <jaepil@appspand.com>
+//
+
+#include <fmt/format.h>
+
+#include <cstdint>
+#include <string_view>
+
+
+int32_t execute_script(const std::string_view& input) {
+    (void)input;
+
+    return 0;
+}
+
 int main(int argc, const char* argv[]) {
-    (void)argc;
-    (void)argv;
+    if (argc < 2) {
+        fmt::print("Usage: expressions <filename>\n");
+        return 1;
+    }
+
+    fmt::print("Executing {}...\n", argv[1]);
+    auto result = execute_script(argv[1]);
+    fmt::print("Result: {}\n", result);
 
     return 0;
 }
